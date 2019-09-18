15d757a9142f4f2ff1bf5d2335ee1c4f3e87d9b1
AE <alinem2104@gmail.com>
2019-09-18 20:28:03 -0300

    Exercicios app de python até RANGE

---
+++ exerc.py
@@ -0,0 +1,75 @@
+# res = 7**4
+# print("Valor: ", res)
+
+# res = 'Hello Python'
+# print(res[::-1])
+# print(res[6:])
+# print(len(res))
+
+# print(res.split())
+# print(res.upper())
+
+# planeta = "Terra"
+# diametro = 12742
+# print("O diâmetro da {} é de {} kilômetros".format(planeta,diametro))
+
+# lst = [1,2,[3,4],[5,[100,200,['olá']],23,11],1,7]
+# print(lst[3][1][2][0])
+
+# lista = [5,3,4,6,1]
+# lista.sort()
+# print(lista)
+
+# d = {'simple_key': 'hello'}
+# print(d['simple_key'])
+
+# d = {'k1': {'k2':'hello'}}
+# print(d['k1']['k2'])
+
+# d = {'k1': [{'nest_key':['this is deep',['hello']]}]}
+# print(d['k1'][0]['nest_key'][1][0])
+
+# d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+# print(d['k1'][2]['k2'][1]['tough'][2][0])
+
+# l = [1,2,6,9,8,1]
+# print(tuple(l))
+
+# l = [1,2,2,33,4,4,11,22,3]
+# l.sort()
+# print(set(l))
+
+# lado_a = 3
+# lado_b = 6
+# lado_c = 9
+# if (lado_a + lado_b >= lado_c) and (lado_a + lado_c >= lado_b) and (lado_b + lado_c >= lado_a):
+#   print("Triângulo válido")
+# else: 
+#   print("Triângulo inválido")
+
+# l = [1,4,5,67,7,8,6,54,4,3,1,21,9,0]
+# for num in l:
+#   if (num % 2 == 0):
+#     print(num)
+
+# string = 'Hello Python'
+# for letter in string[6::]:
+#   print(letter)
+
+# x = 0
+# while x < 5:
+#   print('*' * x)
+#   x += 1
+# while x > 0:
+#   print('*' * x)
+#   x -= 1
+
+# for inteiros in range(1,51):
+#   if (inteiros % 3 == 0 and inteiros % 5 == 0):
+#     print(inteiros, ' - FizzBuzz')
+#   elif (inteiros % 3 == 0):
+#     print(inteiros, ' - Fizz')
+#   elif (inteiros % 5 == 0):
+#     print(inteiros, ' - Buzz')
+
+
